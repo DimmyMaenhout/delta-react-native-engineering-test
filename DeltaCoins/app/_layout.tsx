46aa33a37afc722932de3e40902ca5b2accd9bc8
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-16 13:24:01 +0100

    Create and use the queryClient

--- DeltaCoins/app/_layout.tsx
+++ DeltaCoins/app/_layout.tsx
@@ -1,5 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60,
+      gcTime: 1000 * 60 * 60,
+    },
+  },
+});
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
