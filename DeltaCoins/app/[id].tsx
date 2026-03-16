05fe75283f83d9968ab462863b49dcadd08f78f0
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-16 16:37:16 +0100

    Set navigation title using setOptions + added rightHeader (button) for refreshing the data

--- DeltaCoins/app/[id].tsx
+++ DeltaCoins/app/[id].tsx
@@ -2,52 +2,97 @@ import { fetchCoinDetails } from "@/api/delta/coins";
 import ErrorView from "@/components/errorView";
 import LoadingView from "@/components/loadingView";
 import RowComponent from "@/components/rowComponent";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 import { useQuery } from "@tanstack/react-query";
-import { useLocalSearchParams, Stack } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-
-export const options = {
-  title: "Coin Details",
-};
+import { useLocalSearchParams, Stack, useNavigation } from "expo-router";
+import { useLayoutEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function CoinDetailsScreen() {
+  const navigation = useNavigation();
   const { id } = useLocalSearchParams<{ id: string }>();
 
-  const { data, isPending, error, refetch } = useQuery({
+  const {
+    data,
+    dataUpdatedAt,
+    isPending,
+    isLoading,
+    isFetching,
+    error,
+    refetch,
+  } = useQuery({
     queryKey: ["coin", id],
     queryFn: () => fetchCoinDetails(id),
   });
 
-  console.log("data: ", data);
+  const lastUpdatedDate = new Date(dataUpdatedAt);
 
-  const {
-    name,
-    dirtyCode,
-    priceInUSD,
-    availableSupply,
-    volume24hInUSD,
-    marketCapInUSD,
-  } = data?.data;
+  const title = data?.data?.dirtyCode ? data.data.dirtyCode : "";
 
   function onRetry() {
     refetch();
   }
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Pressable
+          onPress={onRetry}
+          style={(pressed) => {
+            return pressed ? styles.pressed : null;
+          }}
+        >
+          <Ionicons name="refresh" size={24} color="black" />
+        </Pressable>
+      ),
+      title: title,
+    });
+  }, []);
+
+  if (isPending && isFetching && isLoading && data) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <ErrorView
+        message="Something went wrong while fetching the coin details, please try again later."
+        pressed={onRetry}
+      />
+    );
+  }
+
+  if (!data?.data) {
+    return (
+      <View style={styles.root}>
+        <LoadingView message="Fetching coin details..." />
+      </View>
+    );
+  }
+
+  const { name, priceInUSD, availableSupply, volume24hInUSD, marketCapInUSD } =
+    data.data;
+
   return (
     <View style={styles.root}>
-      <Stack.Screen
-        options={{
-          title: dirtyCode ? dirtyCode : isPending ? "Loading..." : "Error",
-        }}
-      />
-
-      {isPending && <LoadingView message="Fetching coin details..." />}
-
-      {error && <ErrorView message="" pressed={onRetry} />}
-
-      {data && !error && (
+      {data && !isFetching && (
         <View>
+          <View style={styles.timeStamp}>
+            <Text>Last updated</Text>
+            <Text>{lastUpdatedDate.toDateString()}</Text>
+          </View>
+
           <RowComponent title={`${name}`} value={`$${priceInUSD}`} />
 
           <RowComponent title="Available" value={availableSupply} />
@@ -64,14 +109,21 @@ export default function CoinDetailsScreen() {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "flex-start",
     textAlign: "center",
+    margin: 16,
   },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
+  },
+  pressed: {
+    opacity: 0.5,
+  },
+  timeStamp: {
+    alignItems: "center",
   },
 });
