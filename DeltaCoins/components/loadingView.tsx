5d02b23668c0b80b213cd6dc52610294b3197861
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2026-03-16 17:40:09 +0100

    Updated the LoadingVIew to just show an ActivityIndicator

--- DeltaCoins/components/loadingView.tsx
+++ DeltaCoins/components/loadingView.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Text } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
-interface LoadingViewProps {
-  message: string;
-}
-
-function LoadingView({ message }: LoadingViewProps) {
-  return <Text>Fetching coin details...</Text>;
+function LoadingView() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator />
+    </View>
+  );
 }
 
 export default LoadingView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+});
